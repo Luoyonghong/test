d2dea986d0deea93a32481f3acbb056d26a41e7f
Luoyonghong <754884172@qq.com>
2018-01-24 23:04:52 +0800

    93

--- src/leetcode87AndLater/Problem93.java
+++ src/leetcode87AndLater/Problem93.java
@@ -18,50 +18,43 @@ public class Problem93
 		return l;
 	}
 	
-	public boolean func(List<Integer> dot,int begin,int num,String s)
+	public void func(List<Integer> dot,int begin,int num,String s)
 	{
 		
-//		if(num==1&&s.length()-begin>3)
-//			return false;
-////		if(dot.size()>3)
-//			return false;
-//		if(begin==s.length)
-//			l.add(new String(r));
-		if(num==1&&begin==s.length())
+		if(num==0&&begin==s.length())
 		{
-//			StringBuffer b=new StringBuffer();
-//			int before=0;
-//			for(int i=1;i<dot.size();i++)
-//			{
-//				b.append(s.substring(before,dot.get(i)+1));
-//				b.append('.');
-//				before=dot.get(i)+1;
-//			}
-//			b.deleteCharAt(b.length()-1);
-//			l.add(b.toString());
-			for (Integer integer : dot)
+			int count=0;
+			String s2="";
+			for(int i=0;i<4;i++)
 			{
-				System.out.print(integer);
+				if(i!=3)
+					s2+=s.substring(count,dot.get(i)+1)+".";
+				else
+					s2+=s.substring(count,dot.get(i)+1);
+				count=dot.get(i)+1;
 			}
-			System.out.println(); 
-			return false;
+			l.add(s2);
+			return ;
 		} 
-		for(int i=1;i<4&&begin+i<s.length();i++)
+		for(int i=0;i<3&&begin+i<s.length();i++)
 		{
-			if(Integer.parseInt(s.substring(begin,begin+i))>255||Integer.parseInt(s.substring(begin,begin+i))<0)
+			String t=s.substring(begin,begin+i+1);
+			if(Integer.parseInt(t)>255||Integer.parseInt(t)<0||(t.length()>1&&t.startsWith("0"))) 
 			{
 				break;
 			}
 			dot.add(begin+i);
-			func(dot, begin+i+1, num--,s);
+			func(dot, begin+i+1, num-1,s);
+			//这里不能用num--或者--num;用--的话，代表func这句话执行完毕之后才减一，则只有遇到func返回的时候才减一，中间递归了很多次了已经，但Num-1则不一样，是一个新的变量了
 			dot.remove(dot.size()-1);
 		}
-		return false;
+		return ;
 	}
 	public static void main(String[] args)
 	{
 		Problem93 p=new Problem93();
-		p.restoreIpAddresses("25525511135");
+		//p.restoreIpAddresses("25525511135");
+		p.restoreIpAddresses("010010");
 //		char[] d={'1','2','3'};
 //		System.out.println(new String(d,1,1));
 		for (String string : p.l)
