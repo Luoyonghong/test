858d22d062ef45d4c705cd5cc553465d1e8a8ea8
Luoyonghong <754884172@qq.com>
2018-02-01 20:00:09 +0800

    complete99

--- src/leetcode87AndLater/Problem99.java
+++ src/leetcode87AndLater/Problem99.java
@@ -3,67 +3,84 @@ package leetcode87AndLater;
 
 public class Problem99
 {
-	
-	class H
-	{
-		int min,max;
-		boolean ok;
-		H(int min,int max,boolean ok)
-		{
-			this.min=min;
-			this.max=max;
-			this.ok=ok;
-		}
-	}
-	public boolean isValidBST(TreeNode root)
-	{
-		
-		return func(root).ok;
-		
-	}
-	
-	public H func(TreeNode h)
-	{
-		if(h==null)
-			return new H(0, 0, true);
-		//System.out.println(h.val); 
-		if(h.left==null&&h.right!=null)
-		{
-			H h1=func(h.left),h2=func(h.right);
-			if(h1.ok&&h2.ok&&h.val<h.right.val&&h2.min>h.val)
-				return new H(h.val,h2.max,true);
-			else
-				return new H(0,0,false);
-		}
-		if(h.left!=null&&h.right==null)
-		{
-			H h1=func(h.left),h2=func(h.right);
-			if(h1.ok&&h2.ok&&h.val>h.left.val&&h1.max<h.val)
-				return new H(h1.min,h.val,true);
-			else
-				return new H(0,0,false);
-		}
-		
-		if(h.left==null&&h.right==null)
-		{
-			return new H(h.val,h.val,true);
-		}
-		
-		if(h.left!=null&&h.right!=null)
-		{
-			H h1=func(h.left),h2=func(h.right);
-			if(h1.ok&&h2.ok&&h.val<h.right.val&&h.val>h.left.val&&h1.max<h.val&&h2.min>h.val)
-				return new H(h1.min,h2.max,true);
-			else
-				return new H(0,0,false);
-		}
-		
-		return new H(0,0,false);
-		
-	}
-	
+	int errorCount=0;
+	int error1,error2;
+	int pre1,pre2;
 	public void recoverTree(TreeNode root)
 	{
-		
+		TreeNode pre=null;
+		int before[]=new int[2];
+		before[0]=Integer.MIN_VALUE;
+		before[1]=Integer.MIN_VALUE;
+		inorder(root, before);
+		if(errorCount==2)
+		{
+			swap(root, error1, error2, error2, error1); 
+		}
+		else
+		{
+			swap(root, error1, pre1, pre1, error1);
+		}
 	}
+	
+	public void inorder(TreeNode t,int[] before)
+	{
+		if(t==null)
+			return;
+		inorder(t.left,before);
+		before[0]=before[1];
+		before[1]=t.val;
+		if(before[1]<before[0])
+		{
+			if(errorCount==0)
+			{
+				errorCount+=1;
+				error1=before[0];
+				pre1=before[1];
+			}
+			else
+			{
+				errorCount+=1;
+				error2=before[1];
+				pre2=before[0];
+			}
+			
+		}
+		inorder(t.right,before); 
+	}
+	
+	public void swap(TreeNode t, int value1, int back1,int value2,int back2)
+	{
+		if(t==null)
+			return;
+		swap(t.left, value1, back1, value2, back2);
+		if(t.val==value1)
+		{
+			t.val=back1;
+		}
+		else if (t.val==value2)
+		{
+			t.val=back2;
+		}
+		swap(t.right, value1, back1, value2, back2);
+	}
+	public static void main(String[] args)
+	{
+		Problem99 p=new Problem99();
+		TreeNode t1=new TreeNode(2),t2=new TreeNode(3),t3=new TreeNode(1);
+		
+		t1.left=t2;
+		t1.right=t3;
+		
+		p.recoverTree(t1);
+		
+		System.out.println(p.error1);
+		System.out.println(p.error2); 
+		
+		System.out.println(t1.left.val);
+		System.out.println(t1.val);
+		System.out.println(t1.right.val);
+	}
+	
+	
 }
