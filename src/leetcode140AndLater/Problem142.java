0994488c0b66e090d07cac114f7ff5a6309cbff2
Luoyonghong <754884172@qq.com>
2018-02-19 17:11:10 +0800

    142

--- src/leetcode140AndLater/Problem142.java
+++ src/leetcode140AndLater/Problem142.java
@@ -2,5 +2,30 @@ package leetcode140AndLater;
 
 public class Problem142
 {
-
+	public ListNode detectCycle(ListNode head)
+	{
+		ListNode fast,slow,r;
+		fast=head;
+		slow=head;
+		r=head;
+		while(true)
+		{
+			if (fast == null || fast.next == null) {
+			            return null;    //遇到null了，说明不存在环
+			        }
+			fast=fast.next.next;
+			slow=slow.next;
+			if(fast==slow)
+				break;
+		}
+		while(r!=null)
+		{
+			if(r==slow)
+				break;
+			r=r.next;
+			slow=slow.next;
+		}
+		return slow;
+		
+	}
 }
