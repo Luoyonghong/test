d9ee7216a37144d1b555163502aff660b7c462e2
Luoyonghong <754884172@qq.com>
2018-02-19 16:44:37 +0800

    141

---
+++ src/leetcode140AndLater/Problem140.java
@@ -0,0 +1,41 @@
+package leetcode140AndLater;
+
+class ListNode
+{
+	int val;
+	ListNode next;
+
+	ListNode(int x)
+	{
+		val = x;
+		next = null;
+	}
+}
+
+public class Problem140
+{
+	public boolean hasCycle(ListNode head)
+	{
+		ListNode r=head;
+		if(head==null)
+			return false;
+		ListNode fast = head;  
+	        ListNode slow = head;  
+	  
+	        while (fast.next != null) {  
+	            fast = fast.next.next;  
+	  
+	            if (fast == null) {  
+	                return false;  
+	            }  
+	  
+	            slow = slow.next;  
+	              
+	            if (fast == slow) {  
+	                return true;  
+	            }  
+	        }  
+	  
+	        return false;  
+	}
+}
