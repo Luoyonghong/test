4ab0a2e97c70a4558121a176656fd1dcf00d45b3
Luoyonghong <754884172@qq.com>
2018-02-24 18:41:46 +0800

    156_2

--- src/leetcode140AndLater/Problem156.java
+++ src/leetcode140AndLater/Problem156.java
@@ -3,4 +3,16 @@ package leetcode140AndLater;
 public class Problem156
 {
 
+	public static void main(String[] args)
+	{
+		try
+		{
+			int a=Integer.parseInt("fs");
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+			//e.printStackTrace();
+		}
+		System.out.println("after catch"); 
+	}
 }
