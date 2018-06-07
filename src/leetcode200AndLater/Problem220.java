ee7470f5a6d38479074a7499ff77e6696868dab2
Luoyonghong <754884172@qq.com>
2018-06-07 21:06:21 +0800

    220

---
+++ src/leetcode200AndLater/Problem220.java
@@ -0,0 +1,22 @@
+package leetcode200AndLater;
+
+import java.util.TreeSet;
+
+public class Problem220
+{
+	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t)
+	{
+		if(k<1||t<0) return false;
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            int n = nums[i];
+            if((set.floor(n)!=null&& n<=t+set.floor(n))||(set.ceiling(n)!=null && set.ceiling(n)<=t+n))
+                return true;
+            set.add(n);
+            if(i>=k)
+                set.remove(nums[i-k]);
+        }
+        return false;
+
+	}
+}
