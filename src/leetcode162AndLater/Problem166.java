e31c06aaa4c455506fef6782b762a7511f96e2d8
Luoyonghong <754884172@qq.com>
2018-06-13 22:12:45 +0800

    166

---
+++ src/leetcode162AndLater/Problem166.java
@@ -0,0 +1,13 @@
+package leetcode162AndLater;
+
+public class Problem166
+{
+	public String fractionToDecimal(int numerator, int denominator)
+	{
+		return null;
+	}	
+	public static void main(String[] args)
+	{
+		System.out.println(2.0/3.0); 
+	}
+}
