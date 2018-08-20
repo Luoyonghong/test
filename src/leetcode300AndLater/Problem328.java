321b168c485a49ce08aca44fd37691582eb06310
Luoyonghong <754884172@qq.com>
2018-08-20 10:33:38 +0800

    328

---
+++ src/leetcode300AndLater/Problem328.java
@@ -0,0 +1,17 @@
+package leetcode300AndLater;
+
+public class Problem328 {
+
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+	// 偶数在前 奇数在后
+	public ListNode oddEvenList(ListNode head) {
+		
+	}
+}
